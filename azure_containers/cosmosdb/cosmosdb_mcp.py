f50e64ca0884c3beca6fc4c329ae21d9af68b18b
Raj Shah (from Dev Box) <shahra@microsoft.com>
2025-05-05 15:22:27 -0700

    Adding tools for vector search hybrid search and reranking

--- azure_containers/cosmosdb/cosmosdb_mcp.py
+++ azure_containers/cosmosdb/cosmosdb_mcp.py
@@ -1,20 +1,23 @@
-from azure.cosmos import CosmosClient
+from azure.cosmos import CosmosClient, ContainerProxy
 from azure.core.paging import ItemPaged
 from typing import Dict, Any, List
 from mcp.server.fastmcp import FastMCP
 from azure.identity import DefaultAzureCredential
-
+from embeddings import generate_embeddings
+import requests
 import os
 
 mcp = FastMCP("cosmosdb")
 
 cosmosClient = None
-account_key = os.getenv("ACCOUNT_KEY")
+ACCOUNT_KEY = os.getenv("ACCOUNT_KEY")
+ACCOUNT_ENDPOINT = os.getenv("ACCOUNT_ENDPOINT")
+EMBEDDING_DIMENSIONS = os.getenv("openai_embeddings_dimensions")
 
-if account_key is not None:
+if ACCOUNT_KEY is not None:
     cosmosClient = CosmosClient(
-        url=os.getenv("ACCOUNT_ENDPOINT"),
-        credential=account_key,
+        url=ACCOUNT_ENDPOINT,
+        credential=ACCOUNT_KEY,
     )
 else:
     cosmosClient = CosmosClient(
@@ -78,6 +81,38 @@ def get_collection_schema(database: str, collection: str):
         print(f"Error retrieving collection schema: {e}")
         return None
     
+# Function to perform vector search in container
+def cdb_vector_search(container_passage: ContainerProxy, query_vector, top_k=5):
+    # Perform vector search
+    return container_passage.query_items( 
+            query='SELECT TOP @top_k c.pid, c.passage, VectorDistance(c.embedding,@embedding) AS SimilarityScore FROM c ORDER BY VectorDistance(c.embedding,@embedding)', 
+            parameters=[ 
+                {"name": "@embedding", "value": query_vector},
+                {"name": "@top_k", "value": top_k},
+            ], enable_cross_partition_query=True)
+
+#Function to perform hybrid search in container
+def cdb_hybrid_search(container_passage: ContainerProxy, query_text, query_vector, top_k=5):
+    query = f'SELECT TOP {top_k} c.pid, c.passage FROM c ORDER BY RANK RRF(FullTextScore(c.passage, {query_text.split()}), VectorDistance(c.embedding, {query_vector}))'
+    return container_passage.query_items(
+            query = query, 
+            enable_cross_partition_query=True)
+
+def perform_reranking(documents: List[str], query: str) -> Dict[str, Any]:
+    """
+    Perform semantic reranking on the given documents based on the query.
+    """
+    try:
+        response = requests.post("https://reranker-api-h2b5czhkfkcphnf4.westus3-01.azurewebsites.net/rerank", 
+                            json={"documents": documents, "query": query, "return_documents": True})
+                            #headers=headers)
+        response.raise_for_status()
+        reranked_documents = response.json()
+        return reranked_documents
+    except Exception as e:
+        print(f"Error performing semantic reranking: {e}")
+        return {"result": [], "error": str(e)}
+
 @mcp.tool(
     name="get_databases",
     description="Get all databases in the Cosmos DB account."
@@ -170,4 +205,85 @@ def get_sample_documents(database: str, container: str, n: int = 1, fields: List
         return {"result": results, "query": f"SELECT TOP {n} c.passage FROM c"}
     except Exception as e:
         print(f"Error retrieving sample document: {e}")
+        return None
+    
+@mcp.tool(
+    name="do_vector_search",
+    description="Get the matching documents using vector search."
+)
+def do_vector_search(database: str, container: str, query: str, top_k: int = 5, similarity_threshold: float = 0.5):
+    """
+    Get the matching documents using vector search.
+    """
+    try:
+        database_proxy = cosmosClient.get_database_client(database)
+        container_proxy = database_proxy.get_container_client(container)
+        query_vector = generate_embeddings(query)
+
+        results = cdb_vector_search(container_proxy, query_vector, top_k)
+        result = []
+        for item in results:
+            if item['SimilarityScore'] >= similarity_threshold:
+                result.append(item['passage'])
+
+        return {"result": result}
+    except Exception as e:
+        print(f"Error retrieving matching documents: {e}")
+        return None
+    
+@mcp.tool(
+    name="do_hybrid_search",
+    description="Get the matching documents using hybrid search."
+)
+def do_hybrid_search(database: str, container: str, query: str, top_k: int):
+    """
+    Get the matching documents using hybrid search.
+    """
+    try:
+        database_proxy = cosmosClient.get_database_client(database)
+        container_proxy = database_proxy.get_container_client(container)
+        query_vector = generate_embeddings(query)
+
+        results = cdb_hybrid_search(container_proxy, query, query_vector, top_k)
+        result = []
+        for item in results:
+            result.append(item['passage'])
+
+        return {"result": result}
+    except Exception as e:
+        print(f"Error retrieving matching documents: {e}")
+        return None
+
+@mcp.tool(
+    name="get_embedding",
+    description="Get the embedding of the specified text."
+)
+def get_embedding(text: str) -> str:
+    """
+    Get the embedding of the specified text.
+    """
+    try:
+        embedding = generate_embeddings(text)
+        if len(embedding) == EMBEDDING_DIMENSIONS:
+            return {"result": embedding}
+        else:
+            return {"error": "Embedding generation using openai large model failed."}
+    except Exception as e:
+        print(f"Error retrieving embedding: {e}")
+        return None
+    
+@mcp.tool(
+    name="get_semantic_reranking",
+    description="Get the semantic reranking for set of documents and specified query."
+)
+def get_semantic_reranking(documents: List[str], query: str) -> str:
+    """
+    Get the semantic reranking for set of documents and specified query.
+    """
+    try:
+        # Assuming documents is a list of strings
+        reranked_documents = perform_reranking(documents, query)
+        return {"result": reranked_documents}
+    except Exception as e:
+        print(f"Error retrieving semantic reranking: {e}")
         return None
