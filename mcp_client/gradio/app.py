b641c39fa1cf5fbb4368664c372564ff8bec2053
Jose Andres Viquez Ramirez <joseandresv@microsoft.com>
2025-05-07 14:49:03 -0600

    Fix

--- mcp_client/gradio/app.py
+++ mcp_client/gradio/app.py
@@ -7,10 +7,18 @@ load_dotenv(dotenv_path=".env")
 
 mcp_client = MCPClientWrapper()
 
+def on_user_change(user: str) -> gr.Chatbot:
+    print(f"User changed to: {user}")
+    messages = mcp_client.load_user_messages(user)
+    print(f"Loaded messages for user {user}: {messages}")
+    return gr.Chatbot(value=messages, height=300, type="messages")
+
 def gradio_interface():
     with gr.Blocks(title="MCP Client") as demo:
         gr.Markdown("# MCP Assistant")
         gr.Markdown("Connect to your MCP server and chat with the assistant")
+
+        dropdown_user = gr.Dropdown(choices=["Mark", "Michelle", "John"], label="Select the user", value="Mark")
         
         with gr.Row(equal_height=True):
             with gr.Column(scale=4):
@@ -26,10 +34,12 @@ def gradio_interface():
         mcp_tools = gr.Textbox(label="MCP tools", interactive=False, max_lines=5)
         
         chatbot = gr.Chatbot(
-            value=[], 
-            height=500,
+            value=mcp_client.load_user_messages(dropdown_user.value), 
+            height=300,
             type="messages"
         )
+
+        dropdown_user.change(on_user_change, inputs=dropdown_user, outputs=chatbot)
         
         with gr.Row(equal_height=True):
             msg = gr.Textbox(
@@ -39,10 +49,10 @@ def gradio_interface():
             )
         
         connect_btn.click(mcp_client.connect, inputs=[server_url, mcp_tools], outputs=mcp_tools)
-        msg.submit(mcp_client.process_message, [msg, chatbot], [chatbot, msg])
+        msg.submit(mcp_client.process_message, [msg, chatbot, dropdown_user], [chatbot, msg])
         
     return demo
 
-if __name__ == "__main__":
-    interface = gradio_interface()
-    interface.launch(debug=True)
+
+interface = gradio_interface()
+interface.launch(server_name="0.0.0.0", server_port=8000)
--- mcp_client/gradio/mcp_client_wrapper.py
+++ mcp_client/gradio/mcp_client_wrapper.py
@@ -2,6 +2,8 @@ import asyncio
 import gradio as gr
 import os
 import json
+import pytz
+import uuid
 
 from contextlib import AsyncExitStack
 from mcp import ClientSession
@@ -9,6 +11,10 @@ from mcp.client.sse import sse_client
 from typing import List, Dict, Any, Union
 from gradio.components.chatbot import ChatMessage
 from openai import AsyncAzureOpenAI
+from azure.cosmos import CosmosClient, ContainerProxy, PartitionKey
+from azure.cosmos.exceptions import CosmosResourceNotFoundError
+from embeddings import generate_embeddings
+from datetime import datetime
 
 class MCPClientWrapper:
     def __init__(self):
@@ -23,14 +29,42 @@ class MCPClientWrapper:
             api_key=os.environ["CHAT_MODEL_API_KEY"],
             api_version=os.environ["CHAT_MODEL_API_VERSION"]
         )
+        self.chat_history_account = CosmosClient(
+            url=os.getenv("COSMOSDB_ACCOUNT_ENDPOINT"),
+            credential=os.getenv("COSMOSDB_ACCOUNT_KEY")
+        )
 
     def connect(self, server_sse_url, mcp_tools):
         return self.loop.run_until_complete(self._connect_mcp_server(server_sse_url, mcp_tools))
     
-    def process_message(self, message: str,  history: List[Union[Dict[str, Any], ChatMessage]]):
+    def process_message(self, message: str,  history: List[Union[Dict[str, Any], ChatMessage]], user: str):
         history.append({"role": "user", "content": message})
-        self.loop.run_until_complete(self._process_query(message, history))
+        self.loop.run_until_complete(self._process_query(message, history, user))
         return history, gr.Textbox(value="")
+    
+    def load_user_messages(self, user: str) -> List[Union[Dict[str, Any], ChatMessage]]:
+        print(f"Loading messages for user {user}...")
+        try: 
+            db = self.chat_history_account.get_database_client("chat_history")
+            container: ContainerProxy = db.get_container_client(user)
+            items = container.query_items(
+                query="SELECT * FROM c ORDER BY c.timestamp ASC",
+                enable_cross_partition_query=True
+            )
+            messages = []
+            for item in items:
+                messages.append({
+                    "role": "user",
+                    "content": item["user_message"]
+                })
+                messages.append({
+                    "role": "assistant",
+                    "content": item["assistant_message"]
+                })
+            return messages
+        except CosmosResourceNotFoundError:
+            print(f"Container for user {user} not found.")
+            return []
     
     async def _connect_mcp_server(self, server_sse_url: str, mcp_tools: str):
         try:
@@ -64,7 +98,14 @@ class MCPClientWrapper:
             mcp_tools += f"Error connecting to MCP server: {server_sse_url.strip('https://').split('/')[0]}\n"
             return mcp_tools
 
-    async def _process_query(self, message: str, history: List[Union[Dict[str, Any], ChatMessage]]):
+    async def _process_query(self, message: str, history: List[Union[Dict[str, Any], ChatMessage]], user: str):
+        similar_message = self._check_similar_message(message, user)
+        if similar_message is not None:
+            print(f"Similar message found: {similar_message}")
+            history.append({"role": "assistant", "content": similar_message})
+            self._store_chat_message(user, message, similar_message)
+            return
+
         while True:
             messages = []
 
@@ -87,12 +128,29 @@ class MCPClientWrapper:
                 temperature=0
             )
 
-            done = await self.process_response_stream(response_stream, history)
+            done = await self.process_response_stream(response_stream, history, message, user)
 
             if done:
                 break
 
-    async def process_response_stream(self, response_stream, history):
+    def _check_similar_message(self, message: str, user: str) -> str:
+        try: 
+            db = self.chat_history_account.get_database_client("chat_history")
+            container: ContainerProxy = db.get_container_client(user)
+            message_embeddings = generate_embeddings(message)
+            items = container.query_items(
+                query="SELECT TOP 1 c.assistant_message, c.timestamp, VectorDistance(c.user_message_embeddings, @embeddings) AS SimilarityScore FROM c WHERE VectorDistance(c.user_message_embeddings, @embeddings) > 0.8 ORDER BY c.timestamp DESC",
+                parameters=[{"name": "@embeddings", "value": message_embeddings}],
+                enable_cross_partition_query=True
+            )
+            message = next(items, None)
+            if message is not None and message["SimilarityScore"] < 0.98:
+                return message["assistant_message"]
+        except CosmosResourceNotFoundError:
+            print(f"Container for user {user} not found.")
+            return None
+
+    async def process_response_stream(self, response_stream, history, message: str, user: str):
         function_arguments = ""
         function_name = ""
         is_collecting_function_args = False
@@ -161,6 +219,21 @@ class MCPClientWrapper:
                     final_content = ''.join([msg for msg in collected_messages if msg is not None])
                     if final_content.strip():
                         history.append({"role": "assistant", "content": final_content})
+                        self._store_chat_message(user, message, final_content)
                         return True
 
         return False  # In case the loop ends without a return, we can handle it here if needed
+
+    def _store_chat_message(self, user: str, user_message: str, assistant_message: str):
+        user_message_embeddings = generate_embeddings(user_message)
+        message = {
+            "id": str(uuid.uuid4()),
+            "user": user,
+            "user_message": user_message,
+            "assistant_message": assistant_message,
+            "user_message_embeddings": user_message_embeddings,
+            "timestamp": datetime.now(tz=pytz.UTC).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        }
+        db = self.chat_history_account.create_database_if_not_exists("chat_history")
+        container: ContainerProxy = db.create_container_if_not_exists(user, partition_key=PartitionKey(path="/user", kind="Hash"))
+        container.create_item(message)
